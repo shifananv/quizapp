df4516e2df74a7e19915e2bc42d8cb90fd74e8d3
shifananv <shifananv2001@gmail.com>
2024-03-12 10:44:10 +0530

    first commit

---
+++ lib/data/questions.dart
@@ -0,0 +1,49 @@
+import 'package:quizapp/models/quiz_question.dart';
+
+ const questions = [
+  QuizQuestion(
+    'What are the main building blocks of Flutter UIs?',
+  [
+    'Widgets',
+    'Components',
+    'Blocks',
+    'Functions',
+  ],
+  ),
+  QuizQuestion(
+    'How are Flutter UIs built?',
+  [
+    'By combining widgets in code',
+    'By combining widgets in a visual editor',
+    'By defining widgets in config files',
+    'By using Xcode for iOS and Amdroid Studio for Android',
+  ],
+  ),
+  QuizQuestion(
+    'What is the purpose of a Statefulwidget?',
+  [
+    'Update UI as data changes',
+    'Update data as UI changes',
+    'Ignore data changes',
+    'None of the above',
+  ],
+  ),
+  QuizQuestion(
+    'What happens if you change data in a StatelessWidget?',
+  [
+    'The UI is not updated',
+    'The UI is updated',
+    'The closest StatefulWidget is updated',
+    'Any nested StatefulWidgets are updated',
+  ],
+  ),
+  QuizQuestion(
+    'How should you update data inside of StatefulWidgets?',
+  [
+    'By calling setState()',
+    'By calling updateData()',
+    'By calling updateUI()',
+    'By calling updateState()',
+  ],
+  ),
+];
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:quizapp/quiz.dart';
+
+
+
+void main() {
+  runApp(
+   const Quiz()
+  );
+}
